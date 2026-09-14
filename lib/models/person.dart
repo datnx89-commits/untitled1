80a4cb396f6f437ee5d6037c5847dddc0447475d
datnx89-commits <datnx89@gmail.com>
2026-09-14 23:07:05 +0700

    slot3a-prm

---
+++ lib/models/person.dart
@@ -0,0 +1,20 @@
+import 'package:untitled1/models/student.dart';
+import 'package:untitled1/models/teacher.dart';
+
+enum PersonType{
+  student,
+  teacher
+}
+
+class Person{
+  final String id;
+  final String name;
+  Person({required this.id, required this.name});
+  factory Person.create({required PersonType personType, required Map<String,dynamic> json}){
+    if(personType == PersonType.student) {
+      return Student.fromJson(json);
+    } else {
+      return Teacher.fromJson(json);
+    }
+  }
+}
---
+++ lib/models/teacher.dart
@@ -0,0 +1,11 @@
+import 'package:untitled1/models/person.dart';
+
+class Teacher extends Person{
+  List<String>? subject;
+  Teacher({required super.id, required super.name, this.subject});
+  factory Teacher.fromJson(Map<String, dynamic> json){
+    return Teacher(id: (json['id']??"") as String,
+        name: (json['name']??"") as String,
+        subject: (json['subject']??[]) as List<String>);
+  }
+}
